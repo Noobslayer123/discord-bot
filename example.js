5359814b9308043bba223348b4d3e11e008edc7b
Noobslayer123 <79126456+Noobslayer123@users.noreply.github.com>
2021-03-06 17:23:04 +0700

    Update example.js

--- example.js
+++ example.js
@@ -1,16 +1,11 @@
 const Discord = require('discord.js')
+const execute = require('./execute.js')
 const client = new Discord.Client()
 const prefix = '!'
 
 client.on('message', async message => {
 message.content = message.content.toLowerCase()
 //code
-
-if (message.content === `${prefix}help`) {
-try {
-message.channel.send(`ok`)
-} catch(err) {
-console.log(`error??`)
-}
-}
+  
+  execute(message)
 })
