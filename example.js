989814abd3f66ab071a93ce2f2f1be314a51c87d
Noobslayer123 <79126456+Noobslayer123@users.noreply.github.com>
2021-03-06 17:25:15 +0700

    Update example.js

--- example.js
+++ example.js
@@ -1,7 +1,10 @@
 const Discord = require('discord.js')
 const execute = require('./execute.js')
+const config = require('./config.js')
 const client = new Discord.Client()
 const prefix = '!'
+
+client.login()
 
 client.on('message', async message => {
 message.content = message.content.toLowerCase()
